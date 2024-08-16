852ab344ccbc4c29e9cce277a872e8620848c271
Rahul C P <141118857+CP-Rahul@users.noreply.github.com>
2024-08-16 09:07:19 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-{
-  "version": 2,
-  "builds": [
-    {
-      "src": "src/index.js",
-      "use": "@vercel/node"
-    }
-  ],
-  "routes": [
-    {
-      "src": "/(.*)",
-      "dest": "src/index.js"
-    }
-  ]
-}
+const express = require('express');
+
+const { ServerConfig } = require('./config');
+const apiRoutes = require('./routes');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use('/api', apiRoutes);
+
+app.listen(ServerConfig.PORT, () => {
+    console.log(`Server is running on port ${ServerConfig.PORT}`);
+})
